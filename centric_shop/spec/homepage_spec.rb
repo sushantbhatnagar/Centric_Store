f3a428271a130332d1ce37033547bb2d421de6a9
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-06-01 01:14:28 +0530

    Adding RSpec tests to the framework for unit tests

---
+++ centric_shop/spec/homepage_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'Landing Page of the Centric Shopping Store' do
+    it 'show display a Centric Logo' do
+    end
+end
---
+++ centric_shop/spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+require 'page-object'
+require 'data_magic'
+require 'fig_newton'
+
+require 'require_all'
+require_all 'lib'
+
+RSpec.configure do |config|
+  config.include PageObject::PageFactory
+
+  # config.before do
+  #   @browser = Watir.Browser.new :chrome
+  # end
+  #
+  # config.after do
+  #   @browser.close
+  # end
+end
