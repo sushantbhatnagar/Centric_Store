229b5951bd184e543fe71990410c0cac2f9547a6
Sushant Bhatnagar <sushantbhatnagar91@gmail.com>
2018-06-01 01:39:37 +0530

    Added a RSpec tests up and running

--- centric_shop/spec/homepage_spec.rb
+++ centric_shop/spec/homepage_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
+require 'page-object'
 
 describe 'Landing Page of the Centric Shopping Store' do
-    it 'show display a Centric Logo' do
-    end
+  it 'show display a Centric Logo' do
+    visit HomePage
+    expect(on(HomePage).header_logo_element).to exist
+  end
 end
--- centric_shop/spec/spec_helper.rb
+++ centric_shop/spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'rspec'
 require 'page-object'
 require 'data_magic'
 require 'fig_newton'
+require 'watir'
 
 require 'require_all'
 require_all 'lib'
@@ -9,10 +10,10 @@ require_all 'lib'
 RSpec.configure do |config|
   config.include PageObject::PageFactory
 
-  # config.before do
-  #   @browser = Watir.Browser.new :chrome
-  # end
-  #
+  config.before do
+    @browser = Watir::Browser.new :chrome
+  end
+
   # config.after do
   #   @browser.close
   # end
