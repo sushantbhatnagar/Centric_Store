099173442819d0341c7e5c50c30d60377845c1c0
sushant.bhatnagar <sushantbhatnagar91@gmail.com>
2023-06-10 13:42:27 -0400

    Dividing feature files into test suites and running it from jenkins pipeline

---
+++ centric_shop/divide_test_case.rb
@@ -0,0 +1,25 @@
+require 'cuke_slicer'
+
+# Number of test suites
+num_suites = 3
+# Get the list of feature files in the directory
+feature_files = Dir.glob("#{Dir.pwd}/features/*.feature")
+
+# Shuffle the feature files randomly
+shuffled_feature_files = feature_files.shuffle
+
+# Divide the shuffled feature files into test suites
+suites = []
+num_suites.times { |i| suites[i] = [] }
+
+shuffled_feature_files.each_with_index do |feature_file, index|
+  suite_index = index % num_suites
+  suites[suite_index] << feature_file
+end
+
+# Print the test suites
+suites.each_with_index do |suite, index|
+  puts "Test Suite #{index + 1}:"
+  suite.each { |feature_file| puts "- #{feature_file}" }
+  puts
+end
