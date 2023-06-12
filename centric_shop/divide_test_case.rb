7ca8bb9c8a768899bb5e5cd7fdd3a46a4aa4b019
sushant.bhatnagar <sushantbhatnagar91@gmail.com>
2023-06-12 12:02:54 -0400

    Updating some scripts to ensure it works

--- centric_shop/divide_test_case.rb
+++ centric_shop/divide_test_case.rb
@@ -1,4 +1,5 @@
 require 'cuke_slicer'
+require 'pry'
 
 # Number of test suites
 num_suites = 3
@@ -7,10 +8,11 @@ feature_files = Dir.glob("#{Dir.pwd}/features/*.feature")
 
 # Shuffle the feature files randomly
 shuffled_feature_files = feature_files.shuffle
-
 # Divide the shuffled feature files into test suites
 suites = []
 num_suites.times { |i| suites[i] = [] }
+
+
 
 shuffled_feature_files.each_with_index do |feature_file, index|
   suite_index = index % num_suites
@@ -21,5 +23,5 @@ end
 suites.each_with_index do |suite, index|
   puts "Test Suite #{index + 1}:"
   suite.each { |feature_file| puts "- #{feature_file}" }
-  puts
+  File.write("suite_#{index + 1}.feature", suite.to_s)
 end
